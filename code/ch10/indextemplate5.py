3d95c35b908ea9cba6aaf70d96cb435130808471
Sebastian Bassi <sbassi@plos.org>
2017-01-30 23:22:19 -0800

    WiP

--- code/ch10/indextemplate5.py
+++ code/ch10/indextemplate5.py
@@ -2,6 +2,6 @@ from bottle import route, run, template
 
 @route('/greets/<name>')
 def shows_greeting(name):
-    return template('index', **{'name':name})
+    return template('new', **{'name':name})
 
 run(host='localhost', port=8000)
---
+++ code/ch10/views/new.html
@@ -0,0 +1,5 @@
+<html lang="en">
+   <body>
+     <h1>Hello {{ name }}!</h1>
+   </body>
+</html>
