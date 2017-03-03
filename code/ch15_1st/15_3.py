b6c979d4ea4a5fa508bb87972a2ecf4b743b648d
Sebastian Bassi <sbassi@plos.org>
2017-03-02 16:54:29 -0800

    15_3

--- code/ch15_1st/15_3.py
+++ code/ch15_1st/15_3.py
@@ -1,20 +1,17 @@
 from Bio import SeqIO
 
 INPUT_FILE = '../../samples/fasta22.fas'
-OUTPUT_FILE = 'fasta22_out.fas'
+OUTPUT_FILE = 'fasta22_out2.fas'
 
 def retseq(seq_fh):
     """
-    Parse a fasta file and store non empty records
-    into the fullseqs list.
+    Parse a fasta file and returns non empty records
     :seq_fh: File handle of the input sequence
-    :return: A list with non empty sequences
+    :return: Non empty sequences
     """
-    fullseqs = []
-    for record in SeqIO.parse(seq_fh,'fasta'):
+    for record in SeqIO.parse(seq_fh, 'fasta'):
         if len(record.seq):
-            fullseqs.append(record)
-    return fullseqs
+            yield record
 
 with open(INPUT_FILE) as in_fh:
     with open(OUTPUT_FILE, 'w') as out_fh:
