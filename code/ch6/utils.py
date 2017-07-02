20c38388b971e6a2cd841fe442cde151803364c3
Sebastian Bassi <sbassi@plos.org>
2017-07-02 10:40:11 -0700

    utils file for chapter 6

---
+++ code/ch6/utils.py
@@ -0,0 +1,6 @@
+# utils.py file
+def save_list(input_list, file_name='temp.txt'):
+    """A list (input_list) is saved to a file (file_name)"""
+    with open(file_name, 'w') as fh:
+        print(*input_list, sep='\n', file=fh)
+    return None
