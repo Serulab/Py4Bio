e96f8d8ac24cb32cc9f18d04d455a2b182f62eae
Sebastian Bassi <sbassi@plos.org>
2017-07-09 22:53:05 -0700

    ch22

--- code/graph/fourcircles.py
+++
@@ -1,8 +0,0 @@
-from bokeh.plotting import figure, output_file, show
-
-p = figure(width=500, height=500)
-x = [1, 1, 2, 2]
-y = [1, 2, 1, 2]
-p.circle(x, y, radius=.35, alpha=0.5, color='red')
-output_file("out.html")
-show(p)
