25a9204bca184e524f038c0574be592113000af3
Murpholinox Peligro <murpholinox@gmail.com>
2019-12-16 17:36:07 -0600

    typo: missing a comma in line 3 of netchargefile.py

--- code/ch5/netchargefile.py
+++ code/ch5/netchargefile.py
@@ -1,6 +1,6 @@
 sequence = ''
 charge = -0.002
-aa_charge = {'C':-.045, 'D':-.999, 'E':-.998, 'H':.091
+aa_charge = {'C':-.045, 'D':-.999, 'E':-.998, 'H':.091,
             'K':1, 'R':1, 'Y':-.001}
 with open('prot.fas') as fh:
    fh.readline()
