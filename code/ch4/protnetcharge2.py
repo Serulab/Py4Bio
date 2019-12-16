3d54c1d8b8effb85c2308e417d9d47bd06b39812
Murpholinox Peligro <murpholinox@gmail.com>
2019-12-16 12:40:28 -0600

    typo: 'aa' instead of aa in line 6 for protnetcharge2.py

--- code/ch4/protnetcharge2.py
+++ code/ch4/protnetcharge2.py
@@ -3,5 +3,5 @@ charge = -0.002
 aa_charge = {'C':-.045,'D':-.999,'E':-.998,'H':.091,
              'K':1,'R':1,'Y':-.001}
 for aa in prot_seq:
-    charge += aa_charge.get('aa', 0)
+    charge += aa_charge.get(aa, 0)
 print(charge)
