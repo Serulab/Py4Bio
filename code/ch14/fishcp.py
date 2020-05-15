4ef0169f589c520be4c746b81fd3cb382d57e065
Sebastian Bassi <sbassi@genesdigitales.com>
2020-05-15 00:22:23 -0700

    updated for new version of Bokeh
    bokeh.charts is deprecated so this version don't use it

--- code/ch14/fishcp.py
+++ code/ch14/fishcp.py
@@ -1,13 +1,24 @@
-from bokeh.charts import Scatter, output_file, show
-from pandas import DataFrame
+from bokeh.plotting import figure, show, output_file
+from bokeh.models.markers import marker_types
+from bokeh.transform import factor_cmap, factor_mark
+from pandas import read_csv
 
-df = DataFrame.from_csv('../../samples/fishdata.csv')
+df = read_csv('../samples/fishdata.csv')
+df['feeds_and_species'] = df['feeds'] + ', ' + df['species']
 
-scatter = Scatter(df, x='PC1', y='PC2', color='feeds',
-        marker='species', title=
-        'Metabolic variations based on 1H NMR profiling of fishes',
-        xlabel='Principal Component 1: 35.8%',
-        ylabel='Principal Component 2: 15.1%')
-scatter.legend.background_fill_alpha = 0.3
+all_markers = [mt for mt in marker_types]
+SPECIES = list(set(df['species']))
+MARKERS = all_markers[:len(SPECIES)]
+feeds = list(set(df['feeds']))
+ttl = 'Metabolic variations based on 1H NMR profiling of fishes'
+p = figure(plot_height=600, plot_width=700, title = ttl)
+p.xaxis.axis_label = 'Principal Component 1: 35.8%'
+p.yaxis.axis_label = 'Principal Component 2: 15.1%'
+p.scatter('PC1', 'PC2', source=df, size=12, fill_alpha=0.3, 
+          marker=factor_mark('species', MARKERS, SPECIES),
+          color=factor_cmap('feeds', 'Category10_3', feeds),
+          legend_field='feeds_and_species')
+p.legend.location = 'top_left'
+p.legend.click_policy = 'hide'
 output_file('scatter.html')
-show(scatter)
+show(p)
